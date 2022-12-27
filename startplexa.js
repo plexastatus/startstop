ba9c228cbe695dafcd6e4c071ae684f777e0b13d
plexastatus <121518901+plexastatus@users.noreply.github.com>
2022-12-27 14:00:36 -0800

    Adding js

---
+++ startplexa.js
@@ -0,0 +1,15 @@
+function myfunction() {
+    var url = "https://prod-21.centralus.logic.azure.com:443/workflows/493f210daeda498d8e2c8f7434ace239/triggers/request/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=eOpgs2zh7jI1rc5hIcfluwZgYbQOuuYFJ1Nf0adbdng";
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Content-Length", "0");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+        }
+    };
+    xhr.send();
+}
