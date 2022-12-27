6d2529389cd178db9194d62b4113c02a6e043382
plexastatus <121518901+plexastatus@users.noreply.github.com>
2022-12-27 15:08:40 -0800

    Update startplexa.js

--- startplexa.js
+++ startplexa.js
@@ -1,6 +1,6 @@
 function myfunction() {
     alert("This is an alert message box.");
-    var url = "https://prod-21.centralus.logic.azure.com:443/workflows/493f210daeda498d8e2c8f7434ace239/triggers/request/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=eOpgs2zh7jI1rc5hIcfluwZgYbQOuuYFJ1Nf0adbdng";
+    var url = "https://prod-21.centralus.logic.azure.com/workflows/493f210daeda498d8e2c8f7434ace239/triggers/request/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Frequest%2Frun&sv=1.0&sig=eOpgs2zh7jI1rc5hIcfluwZgYbQOuuYFJ1Nf0adbdng";
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url);
     xhr.setRequestHeader("Accept", "application/json");
@@ -14,5 +14,5 @@ function myfunction() {
         alert(xhr.readyState);
     };
     xhr.send();
-    alert("This is an alert message box - end.");
+    alert(xhr.readyState);
 }
